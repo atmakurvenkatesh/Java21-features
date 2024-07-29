ca4a0706291600b3a512b0f9953557269d5d731a
venkatesh atmakur <venkatesh atmakur@veatmaku-63146852H>
2024-07-29 17:06:55 +0530

    jars configuration

--- Java21Demo/src/package1/UnNamedClass.java
+++ Java21Demo/src/package1/UnNamedClass.java
@@ -3,3 +3,8 @@ String greeting = "Welcome to world of Java 21";
 void main() {
     System.out.println(greeting);
 }
+
+
+//STEPS TO COMPILE UNNAMED CLASSES
+//javac --release 21 --enable-preview UnNamedClass.java
+//java --enable-preview UnNamedClass
